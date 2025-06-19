1ca6812e64725dfe52a99000a05fb7c12c9574ec
Hasan <hasan@example.com>
2025-06-19 19:11:12 +0300

    Tüm dosyalar eklendi

---
+++ backend/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+async def index():
+    return "<h1>CabbarPay Backend API</h1>"
